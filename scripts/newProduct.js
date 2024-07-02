dcc90ee9de3f93f0a98af658c7b2faf292139d42
Andrew Olson <ols21007@byui.edu>
2024-07-01 20:28:20 -0600

    Added banner

---
+++ scripts/newProduct.js
@@ -0,0 +1,16 @@
+function bannerDay() {
+   const today = new Date().getDay();
+ }
+ 
+ document.addEventListener("DOMContentLoaded", function () {
+   const currentDay = bannerDay();
+   if (currentDay == 1 || currentDay == 2 || currentDay == 3) 
+   {
+     document.getElementById("announcement").display = "flex";
+   }
+ });
+ 
+ function closeBanner() {
+   const banner = document.getElementById("announcement");
+   banner.style.display = "none";
+ }
